0bef3521883b0ac201a596f5d74b2c372881c1c3
Thomas Denewiler <tdenewiler@gmail.com>
2016-02-13 17:35:44 -0800

    Fixed pylint warnings. Using timer in Python talker.

--- src/pylistener.py
+++ src/pylistener.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Example Python node to listen on a specific topic.
+"""
 
 # Import required Python code.
 import rospy
@@ -6,22 +10,28 @@ import rospy
 # Import custom message data.
 from node_example.msg import NodeExampleData
 
-# Create a callback function for the subscriber.
 def callback(data):
+    '''
+    Callback function for the subscriber.
+    '''
     # Simply print out values in our custom message.
     rospy.loginfo(rospy.get_name() + " I heard %s", data.message)
     rospy.loginfo(rospy.get_name() + " a + b = %d", data.a + data.b)
 
-# This ends up being the main while loop.
 def listener():
-    # Create a subscriber with appropriate topic, custom message and name of callback function.
+    '''
+    Main function.
+    '''
+    # Create a subscriber with appropriate topic, custom message and name of
+    # callback function.
     rospy.Subscriber('example', NodeExampleData, callback)
-    # Wait for messages on topic, go to callback function when new messages arrive.
+    # Wait for messages on topic, go to callback function when new messages
+    # arrive.
     rospy.spin()
 
 # Main function.
 if __name__ == '__main__':
     # Initialize the node and name it.
-    rospy.init_node('pylistener', anonymous = True)
+    rospy.init_node('pylistener', anonymous=True)
     # Go to the main loop.
     listener()
--- src/pytalker.py
+++ src/pytalker.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Example Python node to publish on a specific topic.
+"""
 
 # Import required Python code.
 import rospy
-import sys
 
 # Give ourselves the ability to run a dynamic reconfigure server.
 from dynamic_reconfigure.server import Server as DynamicReconfigureServer
@@ -11,52 +14,63 @@ from dynamic_reconfigure.server import Server as DynamicReconfigureServer
 from node_example.msg import NodeExampleData
 from node_example.cfg import nodeExampleConfig as ConfigType
 
-# Node example class.
-class NodeExample():
-    # Must have __init__(self) function for a class, similar to a C++ class constructor.
+class NodeExample(object):
+    '''
+    Node example class.
+    '''
     def __init__(self):
-        # Get the ~private namespace parameters from command line or launch file.
+        # Get the private namespace parameters from command line or launch file.
         init_message = rospy.get_param('~message', 'hello')
         rate = float(rospy.get_param('~rate', '1.0'))
         rospy.loginfo('rate = %d', rate)
         # Create a dynamic reconfigure server.
-        self.server = DynamicReconfigureServer(ConfigType, self.reconfigure)
+        self.server = DynamicReconfigureServer(ConfigType, self.reconfigure_cb)
         # Create a publisher for our custom message.
-        pub = rospy.Publisher('example', NodeExampleData, queue_size=10)
+        self.pub = rospy.Publisher('example', NodeExampleData, queue_size=10)
+        # Initialize message variables.
+        self.int_a = 1
+        self.int_b = 2
+        self.message = init_message
+
+        # Create a timer to go to a callback. This is more accurate than
+        # sleeping for a specified time.
+        rospy.Timer(rospy.Duration(1 / rate), self.timer_cb)
+
+        # Allow ROS to go to all callbacks.
+        rospy.spin()
+
+    def timer_cb(self, _event):
+        """
+        Called at a specified interval. Publishes message.
+        """
         # Set the message to publish as our custom message.
         msg = NodeExampleData()
-        # Initialize message variables.
-        msg.a = 1
-        msg.b = 2
-        msg.message = init_message
-        # Main while loop.
-        while not rospy.is_shutdown():
-            # Fill in custom message variables with values from dynamic reconfigure server.
-            msg.message = self.message
-            msg.a = self.a
-            msg.b = self.b
-            # Publish our custom message.
-            pub.publish(msg)
-            # Sleep for a while before publishing new messages. Division is so rate != period.
-            if rate:
-                rospy.sleep(1/rate)
-            else:
-                rospy.sleep(1.0)
+        # Fill in custom message variables with values updated from dynamic
+        # reconfigure server.
+        msg.message = self.message
+        msg.a = self.int_a
+        msg.b = self.int_b
+        # Publish our custom message.
+        self.pub.publish(msg)
 
-    # Create a callback function for the dynamic reconfigure server.
-    def reconfigure(self, config, level):
-        # Fill in local variables with values received from dynamic reconfigure clients (typically the GUI).
+    def reconfigure_cb(self, config, dummy):
+        '''
+        Create a callback function for the dynamic reconfigure server.
+        '''
+        # Fill in local variables with values received from dynamic reconfigure
+        # clients (typically the GUI).
         self.message = config["message"]
-        self.a = config["a"]
-        self.b = config["b"]
+        self.int_a = config["a"]
+        self.int_b = config["b"]
         # Return the new variables.
         return config
 
-# Main function.    
+# Main function.
 if __name__ == '__main__':
     # Initialize the node and name it.
     rospy.init_node('pytalker')
     # Go to class functions that do all the heavy lifting. Do error checking.
     try:
-        ne = NodeExample()
-    except rospy.ROSInterruptException: pass
+        NodeExample()
+    except rospy.ROSInterruptException:
+        pass
