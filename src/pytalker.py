f50afb8933edc7fa4cd090b1d26dada29a36ebdd
Zongyao Jin <zongyaojin@outlook.com>
2017-08-12 10:47:49 +0800

    change NodeExample's a b to private, and msg.data accessible from CLI

--- src/pytalker.py
+++ src/pytalker.py
@@ -29,9 +29,16 @@ class NodeExample(object):
         self.pub = rospy.Publisher('example', NodeExampleData, queue_size=10)
         # Initialize message variables.
         self.enable = rospy.get_param('~enable', True)
-        self.int_a = rospy.get_param('a', 1)
-        self.int_b = rospy.get_param('b', 2)
+        # Define int_a and int_b also as private variables
+        # such that they can be specified from the command line
+        self.int_a = rospy.get_param('~a', 1)
+        self.int_b = rospy.get_param('~b', 2)
         self.message = rospy.get_param('~message', 'hello')
+
+        # Add ros log information display
+        rospy.loginfo('a = %d', self.int_a)
+        rospy.loginfo('b = %d', self.int_b)
+        rospy.loginfo('message = %s', self.message)
 
         # Create a timer to go to a callback. This is more accurate than
         # sleeping for a specified time.
@@ -51,9 +58,28 @@ class NodeExample(object):
         msg = NodeExampleData()
         # Fill in custom message variables with values updated from dynamic
         # reconfigure server.
-        msg.message = self.message
-        msg.a = self.int_a
-        msg.b = self.int_b
+        # Change message expressions such that they can be also specified using
+        # $ rosparam set /pytalker/a 100
+        # $ rosparam set /pytalker/b 200
+        # $ rosparam set /pytalker/message "Hi"
+        msg.message = rospy.get_param('~message', self.message)
+        msg.a = rospy.get_param('~a', self.int_a)
+        msg.b = rospy.get_param('~b', self.int_b)
+
+        # If changes are detected, display ros log informing the changes
+        if self.message != msg.message:
+            rospy.loginfo('new message = %s', msg.message)
+        if self.int_a != msg.a:
+            rospy.loginfo('new a = %d', msg.a)
+        if self.int_b != msg.b:
+            rospy.loginfo('new b = %d', msg.b)
+
+        # Assign private variables to newly assigned values such that
+        # the notification about change won't come up in every callback
+        self.message = msg.message
+        self.int_a = msg.a
+        self.int_b = msg.b
+
         # Publish our custom message.
         self.pub.publish(msg)
 
