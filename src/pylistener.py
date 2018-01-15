920d6812c294741a3ff68979d3537b8743225bae
Thomas Denewiler <tdenewiler@gmail.com>
2018-01-15 14:23:18 -0800

    Cleaning up Python code with the use of new linters including pep8, pep257, and pyflakes.

--- src/pylistener.py
+++ src/pylistener.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Example Python node to listen on a specific topic.
-"""
+"""Example Python node to listen on a specific topic."""
 
 # Import required Python code.
 import rospy
@@ -10,24 +8,20 @@ import rospy
 # Import custom message data.
 from node_example.msg import NodeExampleData
 
+
 def callback(data):
-    '''
-    Callback function for the subscriber.
-    '''
+    """Handle subscriber data."""
     # Simply print out values in our custom message.
     rospy.loginfo(rospy.get_name() + " I heard %s", data.message +
                   ", a + b = %d" % (data.a + data.b))
 
+
 def listener():
-    '''
-    Main function.
-    '''
+    """Configure subscriber."""
     # Create a subscriber with appropriate topic, custom message and name of
     # callback function.
     rospy.Subscriber('example', NodeExampleData, callback)
-    # Wait for messages on topic, go to callback function when new messages
-    # arrive.
-    rospy.spin()
+
 
 # Main function.
 if __name__ == '__main__':
@@ -35,3 +29,6 @@ if __name__ == '__main__':
     rospy.init_node('pylistener')
     # Go to the main loop.
     listener()
+    # Wait for messages on topic, go to callback function when new messages
+    # arrive.
+    rospy.spin()
