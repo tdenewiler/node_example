1079a7a4db670293b5a99e7f0838e528262ac54b
Thomas Denewiler <tdenewiler@gmail.com>
2020-03-26 09:59:56 -0700

    Updating python code with black formatter. Checking with Statick black plugin. (#33)

--- src/pylistener.py
+++ src/pylistener.py
@@ -12,21 +12,22 @@ from node_example.msg import NodeExampleData
 def callback(data):
     """Handle subscriber data."""
     # Simply print out values in our custom message.
-    rospy.loginfo("%s: I heard %s, a + b = %d", rospy.get_name(), data.message,
-                  data.a + data.b)
+    rospy.loginfo(
+        "%s: I heard %s, a + b = %d", rospy.get_name(), data.message, data.a + data.b
+    )
 
 
 def listener():
     """Configure subscriber."""
     # Create a subscriber with appropriate topic, custom message and name of
     # callback function.
-    rospy.Subscriber('example', NodeExampleData, callback)
+    rospy.Subscriber("example", NodeExampleData, callback)
 
 
 # Main function.
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Initialize the node and name it.
-    rospy.init_node('pylistener')
+    rospy.init_node("pylistener")
     # Go to the main loop.
     listener()
     # Wait for messages on topic, go to callback function when new messages
